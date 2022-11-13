157ed6caa9e08649827704bdc97309eba9c1ea89
Prabesh Thapa <numb1prabesht7@gmail.com>
2022-11-13 14:34:51 +0545

    added about and skills page

--- components/About.tsx
+++ components/About.tsx
@@ -1,13 +1,18 @@
-import React from 'react'
+import React from 'react';
+import Image from 'next/image';
+import Link from 'next/link';
+import AboutImg from '../public/assets/about.jpg';
 
 const About = () => {
   return (
-    <div className='w-full md:h-screen p-2 flex item-center py-16'>
-        <div className='max-w-[1240px] m-auto md:grid grid-cols-3 gap-8'>
-            <div className='col-span-2'>
-                <p className=''>About</p>
-                <p>Who I am</p>
-                <p className='py-2 text-gray-600'>
+    <div id='about' className='w-full md:h-screen p-2 flex items-center py-16'>
+      <div className='max-w-[1240px] m-auto md:grid grid-cols-3 gap-8'>
+        <div className='col-span-2'>
+          <p className='uppercase text-xl tracking-widest text-[#5651e5]'>
+            About
+          </p>
+          <h2 className='py-4'>Who I Am</h2>
+          <p className='py-2 text-gray-600'>
             I specialize in building mobile responsive front-end UI applications
             that connect with API’s and other backend technologies. I’m
             passionate about learning new technologies and understand there is
@@ -25,13 +30,18 @@ const About = () => {
             time I run Code Commerce, a Youtube channel where I teach web
             developement and various front-end technologies.
           </p>
-                <div>
-
-                </div>
-            </div>
+          <Link href='/#projects'>
+            <p className='py-2 text-gray-600 underline cursor-pointer'>
+              Check out some of my latest projects.
+            </p>
+          </Link>
         </div>
+        <div className='w-full h-auto m-auto shadow-xl shadow-gray-400 rounded-xl flex items-center justify-center p-4 hover:scale-105 ease-in duration-300'>
+          <Image src={AboutImg} className='rounded-xl' alt='/' />
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default About
+export default About;
